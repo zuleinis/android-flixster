112c59d44fabc77068df82a5e08cd54f511fd345
Zuleinis <zuleinis.ramos@upr.edu>
2021-12-03 03:50:14 -0400

    add youtube player view

--- app/src/main/java/com/codepath/flixster/models/Movie.java
+++ app/src/main/java/com/codepath/flixster/models/Movie.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Parcel
 public class Movie {
 
+    int movieId;
     String posterPath;
     String title;
     String overview;
@@ -24,6 +25,7 @@ public class Movie {
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
         rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -48,5 +50,9 @@ public class Movie {
 
     public double getRating() {
         return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
